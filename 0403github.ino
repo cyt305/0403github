40e3d123cc3aef896129eaff3e25bfa66672a305
cyt305 <118765945+cyt305@users.noreply.github.com>
2023-04-03 19:00:53 -0400

    add buttonPin

--- 0403github.ino
+++ 0403github.ino
@@ -2,21 +2,27 @@ int led = 32;
 int led2 = 31;
 int pot = A14;
 int delayTime;
+int buttonPina = 33;
+
 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   pinMode(led2, OUTPUT);
+  pinMode(buttonPina, INPUT);
+
 }
 
 void loop() {
-  delayTime = analogRead(pot);
-  digitalWrite(led, HIGH);
-  digitalWrite(led2, HIGH);
+  if (digitalRead(buttonPina) == HIGH) {
 
-  delay(500);
-  digitalWrite(led, LOW);
-  digitalWrite(led2, HIGH);
+    delayTime = analogRead(pot);
+    digitalWrite(led, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(delayTime);
+    digitalWrite(led, LOW);
+    digitalWrite(led2, HIGH);
+  }
 
 }
